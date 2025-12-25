6c5a640de8876de6d4bfec4fd62fa22cd0b64d49
cheng-hsiang-chiu <58955345+cheng-hsiang-chiu@users.noreply.github.com>
2025-12-24 23:41:28 -0600

    Refactor user questions to tourist questions

--- Paris_tour_guide.py
+++ Paris_tour_guide.py
@@ -1,4 +1,3 @@
-# Start your code here!
 import os
 from openai import OpenAI
 
@@ -8,14 +7,19 @@ model = "gpt-4o-mini"
 # Define the client
 client = OpenAI()
 
-# Start coding here
-# Add as many cells as you like
-user_questions = ["How far away is the Louvre from the Eiffel Tower (in miles) if you are driving?","Where is the Arc de Triomphe?","What are the must-see artworks at the Louvre Museum?"]
+# Define three questions from tourists
+tourist_questions = ["How far away is the Louvre from the Eiffel Tower (in miles) if you are driving?","Where is the Arc de Triomphe?","What are the must-see artworks at the Louvre Museum?"]
+
+# Define the conversation to OpenAI client
 conversation=[{"role":"system","content":"You are a Parisian expert, delivering valuable insights into the city's iconic landmarks and hidden treasures."}]
 
-for q in user_questions:
+for q in tourist_questions:
     print(q)
+    
     conversation.append({"role":"user","content":q})
+
+    # Send the messages with temperature and maximum output tokens to the client
+    # temperature 0.0 means lowest randomness response
     response = client.chat.completions.create(
         model=model,
         messages=conversation,
@@ -23,11 +27,15 @@ for q in user_questions:
         max_completion_tokens=100
     )
 
+    # Extract the response and append to conversation as an "assistant"
     conversation.append({"role":"assistant","content":response.choices[0].message.content})
+
+    # Send the new messages again 
     response = client.chat.completions.create(
         model=model,
         messages=conversation,
         temperature=0.0,
         max_completion_tokens=100
     )
+    
     print(response.choices[0].message.content)
