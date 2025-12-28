c7249aef9ee1d18c0dd083d5b74d513cba59a087
cheng-hsiang-chiu <58955345+cheng-hsiang-chiu@users.noreply.github.com>
2025-12-27 23:34:18 -0600

    Add Embeddings.py to obtain embeddings from OpenAI

---
+++ Embeddings.py
@@ -0,0 +1,15 @@
+from openai import OpenAI
+
+# Create an OpenAI client
+client = OpenAI(api_key="<OPENAI_API_TOKEN>")
+
+# Create a request to obtain embeddings
+response = client.embeddings.create(
+    model="text-embedding-3-small",
+    input="what is the embedding?"
+)
+
+# Convert the response into a dictionary
+response_dict = response.model_dump()
+
+print(response_dict)
