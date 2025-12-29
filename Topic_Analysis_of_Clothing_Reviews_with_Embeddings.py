1ff4fbbd9c5726d298082b335d3e0276bf582e20
cheng-hsiang-chiu <58955345+cheng-hsiang-chiu@users.noreply.github.com>
2025-12-29 09:34:47 -0600

    Create Topic_Analysis_of_Clothing_Reviews_with_Embeddings.py

---
+++ Topic_Analysis_of_Clothing_Reviews_with_Embeddings.py
@@ -0,0 +1,4 @@
+# Project Description : 
+# In the competitive world of e-commerce, understanding customer feedback is vital for business success. This project focuses on analyzing reviews from the Women's Clothing E-Commerce Reviews dataset. 
+# The challenge is to process and analyze text data, categorize feedback into themes, and perform semantic and similarity searches to gain deeper understanding of customer sentiments.
+
